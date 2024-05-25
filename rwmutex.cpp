5b53eb37e4b37b7047c8e82fca0405bc57c93651
wwt13 <qq1270414897@gmail.com>
2024-05-25 10:01:46 +0800

    rwmutex.cpp生动形象的展示了线程运行的可能性

--- rwmutex.cpp
+++ rwmutex.cpp
@@ -2,6 +2,7 @@
 #include<mutex>
 #include<thread>
 #include<chrono>
+#include<vector>
 #include<shared_mutex>
 
 using namespace std;
@@ -11,7 +12,7 @@ string book="EMPTY";
 
 void read(int i){
 	shared_lock<shared_mutex> lock(s_m);
-	this_thread::sleep_for(chrono::seconds(2));
+	// this_thread::sleep_for(chrono::seconds(2));
 	cout<< book << i << endl;
 	// 共享锁会在超出锁作用域的时候自动释放
 }
@@ -19,19 +20,25 @@ void read(int i){
 void write(){
 	unique_lock<shared_mutex> lock(s_m);
 	book="WUJIAYUAN's BEAUTY";
-	this_thread::sleep_for(chrono::seconds(4));
+	// this_thread::sleep_for(chrono::seconds(4));
 }
 
 int main(){
-	thread thw1(write);
-	thread thr1(read,1);
-	thread thr2(read,2);
-	thread thr3(read,3);
+	vector<thread> threads;
 
-	thw1.detach();
-	thr1.detach();
-	thr2.detach();
-	thr3.detach();
+	threads.emplace_back(write);
+
+	for(int i=1;i<=3;i++){
+		threads.emplace_back(read,i);
+	}
+
+	// join和detach的区别在于主线程是否需要阻塞
+	// 之前理解错了，对于线程本身而言，join和detach都是并发调度
+	for(auto &th:threads){
+		th.join();
+	}
+
+	cout<< "如果这句话出现在输出末尾，证明主线程阻塞，是join调用子线程的方法" << endl;
 
 	while(true);
 	return 0;
